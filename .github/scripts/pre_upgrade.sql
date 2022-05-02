23afd291919d22fa1bad35a2cd6c7dee1f16a930
Rishabh Tanwar <ritanwar@amazon.com>
2022-05-02 17:43:26 +0000

    Remove pre/post upgrade scripts
    Signed-off-by: Rishabh Tanwar <ritanwar@amazon.com>

--- .github/scripts/pre_upgrade.sql
+++
@@ -1,4 +0,0 @@
-ALTER EXTENSION babelfishpg_common DROP CAST (boolean as sys.bpchar);
-ALTER EXTENSION babelfishpg_common DROP CAST (boolean as sys.varchar);
-DROP CAST (boolean as sys.bpchar);
-DROP CAST (boolean as sys.varchar);
