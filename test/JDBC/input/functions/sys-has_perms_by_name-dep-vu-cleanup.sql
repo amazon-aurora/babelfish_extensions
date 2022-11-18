503637163df2daad0706902df07f5ae97623e3e2
vasavi suthapalli <svasusri@amazon.com>
2022-11-18 07:36:39 +0000

    Remove dep test file and create vu-prepare/verify similar to previous existing sys-has_perms_by_name test file and add case insensitivity checks

--- test/JDBC/input/functions/sys-has_perms_by_name-dep-vu-cleanup.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE t_perms_by_name
-GO
-
-DROP FUNCTION has_perms_by_name_func
-GO
--- test/JDBC/input/functions/sys-has_perms_by_name-dep-vu-verify.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE t_perms_by_name (col1 INT, col2 VARCHAR(16));
-GO
-
-SELECT has_perms_by_name_func('dbo.t_perms_by_name','OBJECT', 'SELECT', 'col2', 'COLUMN');
-GO
-
-SELECT has_perms_by_name_func('dBo.t_PeRmS_bY_nAmE','oBjEcT', 'sElEcT', 'CoL2', 'cOlUmN');
-GO
