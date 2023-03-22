2411cca52e36d717e4822ecc589ec23e56585070
Kristian Lejao <klejao@amazon.com>
2023-03-22 18:17:47 +0000

    [BABEL-3238] Guard elog in get_tsql_error_details()
    Signed-off-by: Kristian Lejao <klejao@amazon.com>

--- contrib/babelfishpg_tds/src/backend/tds/err_handler.c
+++ contrib/babelfishpg_tds/src/backend/tds/err_handler.c
@@ -193,8 +193,10 @@ get_tsql_error_details(ErrorData *edata,
 
 		TDSInstrumentation(INSTR_TDS_UNMAPPED_ERROR);
 
-		elog(LOG, "Unmapped error found. Code: %d, Message: %s, File: %s, Line: %d, Context: %s",
-			 edata->sqlerrcode, edata->message, edata->filename, edata->lineno, error_context);
+		// Not allowed to elog if stack depth is reached
+		if (!err_stack_full())
+			elog(LOG, "Unmapped error found. Code: %d, Message: %s, File: %s, Line: %d, Context: %s",
+					edata->sqlerrcode, edata->message, edata->filename, edata->lineno, error_context);
 
 		return false;
 	}
