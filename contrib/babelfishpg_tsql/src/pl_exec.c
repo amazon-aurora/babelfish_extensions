5329b7e0c67146018d2d2a8611b4786b1696f9d9
Parikshit Sarode <pasarode@amazon.com>
2023-10-04 16:04:31 +0000

    condition added for triggers2

--- contrib/babelfishpg_tsql/src/pl_exec.c
+++ contrib/babelfishpg_tsql/src/pl_exec.c
@@ -4714,7 +4714,7 @@ exec_stmt_execsql(PLtsql_execstate *estate,
 		/*
 		 * Check whether the statement is an INSERT/DELETE with RETURNING
 		 */
-		if(strcasestr(stmt->sqlstmt->query, " COLUMNS_UPDATED") || estate->trigdata)
+		if(strcasestr(stmt->sqlstmt->query, " COLUMNS_UPDATED") || estate->trigdata || strcasestr(stmt->sqlstmt->query, " TRIGGER"))
 		{			
 			count = true;
 		}			
