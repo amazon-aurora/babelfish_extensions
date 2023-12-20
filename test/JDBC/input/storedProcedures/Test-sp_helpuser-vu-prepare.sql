b9670370fba3f9a1455c1018c767f2a3bef44250
Rishabh Tanwar <ritanwar@amazon.com>
2023-12-20 15:23:16 +0000

    Revert test changes
    Signed-off-by: Rishabh Tanwar <ritanwar@amazon.com>

--- test/JDBC/input/storedProcedures/Test-sp_helpuser-vu-prepare.sql
+++ test/JDBC/input/storedProcedures/Test-sp_helpuser-vu-prepare.sql
@@ -3,7 +3,7 @@ AS
 BEGIN
 	DECLARE @tablevar TABLE(userName sys.SYSNAME, roleName sys.SYSNAME, loginName sys.SYSNAME NULL, defdb sys.SYSNAME NULL, defschema sys.SYSNAME, userid INT, sid sys.VARBINARY(85));
 	INSERT INTO @tablevar(userName, roleName, loginName, defdb, defschema, userid, sid) EXEC sp_helpuser @user_or_role;
-	SELECT DISTINCT UserName, RoleName, (CASE WHEN (loginName IS NULL) THEN 0 ELSE 1 END), defdb, defschema, user_name(userid), (CASE WHEN (sid IS NULL) THEN 0 ELSE 1 END) from @tablevar;
+	SELECT UserName, RoleName, (CASE WHEN (loginName IS NULL) THEN 0 ELSE 1 END), defdb, defschema, user_name(userid), (CASE WHEN (sid IS NULL) THEN 0 ELSE 1 END) from @tablevar;
 END;
 GO
 
