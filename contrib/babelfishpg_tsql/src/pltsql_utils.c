9bcb97f1336d0720ff23b077f5fe4ebb8b05e5cb
Tim Chang <changti@amazon.com>
2024-04-23 22:07:18 +0000

    Add tests for ENR temp table transactions
    Signed-off-by: Tim Chang <changti@amazon.com>
    
    Rerun flaky test
    
    Added tests for ENR drop + recreate in transaction
    
    Signed-off-by: Tim Chang <changti@amazon.com>
    
    Added additional test cases covering indexes
    
    Signed-off-by: Tim Chang <changti@amazon.com>
    
    Expand test coverage, add GUC
    
    Move ENR Rollback calls to xact_callbacks, added subtransaction support
    and tests
    
    Signed-off-by: Tim Chang <changti@amazon.com>
    
    Move rollback calls into xact.c; add isolation snapshot and xact abort
    tests
    
    Signed-off-by: Tim Chang <changti@amazon.com>
    
    Added additional tests
    
    Signed-off-by: Tim Chang <changti@amazon.com>

--- contrib/babelfishpg_tsql/src/pltsql_utils.c
+++ contrib/babelfishpg_tsql/src/pltsql_utils.c
@@ -20,6 +20,7 @@
 #include "utils/fmgroids.h"
 #include "utils/catcache.h"
 #include "utils/acl.h"
+#include "utils/queryenvironment.h"
 #include "access/table.h"
 #include "access/genam.h"
 #include "catalog.h"
---
+++ test/JDBC/expected/temp_table_rollback-vu-cleanup.out
@@ -0,0 +1,11 @@
+DROP VIEW enr_view
+GO
+
+DROP TYPE temp_table_type
+GO
+
+DROP PROCEDURE test_rollback_in_proc
+GO
+
+DROP PROCEDURE implicit_rollback_in_proc
+GO
