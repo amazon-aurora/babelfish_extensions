246d75443c04ec99d7a0371771969290eaa2d18d
ANJU BHARTI <abanju@amazon.com>
2025-12-15 08:55:18 +0000

    changes in makefile

--- contrib/babelfishpg_common/sql/babelfishpg_common--5.4.0--6.0.0.sql
+++
@@ -1,7 +0,0 @@
-\echo Use "ALTER EXTENSION ""babelfishpg_common"" UPDATE TO "6.0.0"" to load this file. \quit
-SELECT set_config('search_path', 'sys, '||current_setting('search_path'), false);
-
--- Placeholder for 6.0.0 upgrade changes
--- Add upgrade logic here as needed
-
-SELECT set_config('search_path', trim(leading 'sys, ' from current_setting('search_path')), false);
